d2359c8670a2566bfcdbf278ff801d326edb1034
mihaque <haque.c.k@gmail.com>
2022-10-18 10:24:28 +0530

    Update merge_with_final.sql

--- sql/merge_with_final.sql
+++ sql/merge_with_final.sql
@@ -1,5 +1,5 @@
-MERGE INTO `PROJECT_ID.hopkins_covid.hopkins_covid_final` FINAL
-USING `PROJECT_ID.hopkins_covid.hopkins_covid_staging` STAGING
+MERGE INTO `PROJECT_ID.hopkins_data.hopkins_final` FINAL
+USING `PROJECT_ID.hopkins_data.hopkins_staging` STAGING
 ON (
     STAGING.date = FINAL.date AND STAGING.country_region = FINAL.country_region AND STAGING.province_state = FINAL.province_state
   )
